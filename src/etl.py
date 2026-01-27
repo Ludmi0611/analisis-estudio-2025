a3091470b537f5d0574f0a836f8b3c3f85fffa93
Ludmila Andrea Sánchez <ludsanchez696@gmail.com>
2026-01-26 23:25:24 -0300

    update etl.py

--- src/etl.py
+++ src/etl.py
@@ -9,3 +9,77 @@ cursos_2025 = RAW_DIR / "cursos_2025.csv"
 cursos_inscripcion_2025 = RAW_DIR / "cursos_inscripcion_2025.csv"
 horas_estudio = RAW_DIR / "horas_estudio.csv"
 
+df_cursos_2025 = pd.read_csv(cursos_2025)
+df_cursos_inscripcion_2025 = pd.read_csv(cursos_inscripcion_2025)
+df_horas_estudio = pd.read_csv(horas_estudio, header=1)
+
+# print(df_cursos_2025.head())
+# print(df_cursos_2025.info())
+
+# print(df_cursos_inscripcion_2025.head())
+# print(df_cursos_inscripcion_2025.info())
+
+# print(df_horas_estudio.head())
+# print(df_horas_estudio.info())
+
+def clean_columns(df):
+    df = df.copy()
+    df.columns = (
+        df.columns
+        .str.strip()
+        .str.lower()
+        .str.replace(" ", "_")
+    )
+    return df
+
+def clean_text(df):
+    df = df.copy()
+    for col in df.select_dtypes(include=["object", "string"]).columns:
+        df[col] = (
+            df[col]
+            .astype("string")
+            .str.strip()
+            .str.title()
+            .replace("Nan", pd.NA)
+        )
+    return df
+
+df_objetivos = clean_columns(df_cursos_2025)
+df_inscripcion = clean_columns(df_cursos_inscripcion_2025)
+df_estudio = clean_columns(df_horas_estudio)
+df_objetivos = clean_text(df_objetivos)
+df_inscripcion = clean_text(df_inscripcion)
+df_estudio = clean_text(df_estudio)
+print(df_objetivos.head())
+
+print(df_inscripcion.head())
+print(df_estudio.head())
+
+df_objetivos["fecha_de_inicio"] = pd.to_datetime(
+    df_objetivos["fecha_de_inicio"], 
+    dayfirst=True,
+    errors="coerce"
+)
+
+df_objetivos["fecha_de_finalización"] = pd.to_datetime(
+    df_objetivos["fecha_de_finalización"],
+    dayfirst=True,
+    errors="coerce"
+)
+
+df_estudio["dia"] = pd.to_datetime(
+    df_estudio["dia"],
+    dayfirst=True,
+    errors="coerce"
+)
+
+# !! corregir fecha de inscripcion ya que no le puse año, y tiene texto !!
+df_inscripcion["fecha"] = pd.to_datetime(
+    df_inscripcion["fecha"],
+    dayfirst=True,
+    errors="coerce"
+)
+print(df_objetivos.head())
+# print(df_objetivos.isna().sum())
+print(df_estudio.head())
+print(df_inscripcion.head())
